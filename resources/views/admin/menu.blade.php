0146c076add4514b6d4ab7d44896e3d53d73e996
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-23 15:20:47 +0300

    added admin panel html and css

---
+++ resources/views/admin/menu.blade.php
@@ -0,0 +1,181 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Auto park</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user">&nbsp;</span>Hello Admin</a></li>
+                <li class="active"><a title="View Website" href="#"><span class="glyphicon glyphicon-globe"></span></a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid">
+    <div class="col-md-3">
+
+        <div id="sidebar">
+            <div class="container-fluid tmargin">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search..." />
+                    <span class="input-group-btn">
+              <button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+          </span>
+                </div>
+            </div>
+
+            <ul class="nav navbar-nav side-bar">
+                <li class="side-bar tmargin"><a href="#"><span class="fa fa-car">&nbsp;</span> Cars</a></li>
+                <li class="side-bar">
+                    <a href="#"><span><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;</span> Drivers</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-tint">&nbsp;</span>Fuel</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-road">&nbsp;</span>Routes</a></li>
+
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-user">&nbsp;</span>Users</a></li>
+                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-signal">&nbsp;</span>Statistics</a></li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-9 animated bounce">
+        <h1 class="page-header">Dashboard</h1>
+        <ul class="breadcrumb">
+            <li><span class="glyphicon glyphicon-home">&nbsp;</span>Home</li>
+            <li><a href="#">Dashboard</a></li>
+        </ul>
+        <table class="table table-hover">
+            <thead>
+            <th>&nbsp;</th>
+            <th class="text-center">#</th>
+            <th>Title</th>
+            <th class="text-center">Author</th>
+            <th>Status</th>
+
+            </thead>
+            <tbody>
+            <!--
+              <tr>
+                <td><input type="checkbox" /></td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-info">Pending</span></td>
+              </tr>
+    -->
+            <!-- START CONTENT END -->
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-info"><span class="glyphicon glyphicon-time">&nbsp;</span>Pending</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" />
+                </td>
+                <td class="text-center">1</td>
+                <td width="70%">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
+                <td class="text-center" width="10%">Admin</td>
+                <td><span class="label label-success"><span class="glyphicon glyphicon-ok-sign">&nbsp;</span>Live</span>
+                </td>
+            </tr>
+
+            <!-- DUMP CONTENT END -->
+
+            </tbody>
+
+        </table>
+    </div>
+</div>
+
+</div>
--- resources/views/admin/style.blade.php
+++ resources/views/admin/style.blade.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kestas Subacius
- * Date: 8/22/2017
- * Time: 3:04 PM
- */
+<link rel="stylesheet" href="{{ URL::asset('css/menu.style.css') }}">

ffd30fe8cdfca2833c29c4df7fd10560d49113b1
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-23 11:14:36 +0300

    added generate drivers with faker

--- app/Console/Commands/UsersFaker.php
+++ app/Console/Commands/UsersFaker.php
@@ -44,48 +44,42 @@ class UsersFaker extends Command
         $this->userData();
     }
 
-    public function userData(){
+    public function userData()
+    {
 
         $faker = Factory::create();
 
         for ($i = 0; $i < 500; $i++) {
 
             $persons = null;
-            while(!$persons){
+            while (!$persons) {
 
-                $firstNumbers = $faker->numberBetween($min = 3, $max = 4 );
-                $btime = Carbon::create(rand(1980, 2000), rand(1, 12), rand(1, 31), rand(0, 23), rand(0, 59), rand(0, 59))->format('Ymd');
-                $cutDate = substr($btime, -6);
+                $firstNumbers = $faker->numberBetween($min = 3, $max = 4);
+                $bDate = substr(Carbon::create(rand(1980, 2000), rand(1, 12), rand(1, 31), rand(0, 23), rand(0, 59), rand(0, 59))->format('Ymd'), -6);
+                $fourDigits = (str_pad($faker->numberBetween($min = 0, $max = 9999), 4, '0', STR_PAD_LEFT));
 
-                $fourDigits = str_pad($faker->numberBetween($min = 0, $max = 9999), 4, '0',STR_PAD_LEFT);
+                $persons = $firstNumbers . $bDate . $fourDigits;
 
-                $persons = $firstNumbers.$cutDate.$fourDigits;
-
-                $personInt = (int)$persons;
-//                dd($personInt);
-//
-//                $float = (float)$persons;
-//
-//                dd(floor($float));
-//                $num = "314";
-//                $int = (int)$num;
-//                dd($int);
-
-
-
-
-
-                if(ApUsers::where('person_id', $persons)->get()->isNotEmpty())
+                if (ApUsers::where('person_id', $persons)->get()->isNotEmpty())
                     $persons = null;
             }
 
-           $record = ApUsers::create([
+            $phone_number = null;
+            while (!$phone_number) {
+                $phone_number = '86' . str_pad($faker->numberBetween($min = 0, $max = 9999999), 7, '0', STR_PAD_LEFT);
+
+
+                if (ApUsers::where('phone', $phone_number)->get()->isNotEmpty())
+                    $phone_number = null;
+            }
+
+            $record = ApUsers::create([
                 'id' => Uuid::uuid4(),
                 'name' => $faker->firstName,
                 'surname' => $faker->lastName,
                 'residential_address' => $faker->address,
-                'person_id' => $personInt,
-                'phone' => $faker->phoneNumber,
+                'person_id' => $persons,
+                'phone' => $phone_number,
                 'email' => $faker->email,
                 'password' => 'test12',
             ]);

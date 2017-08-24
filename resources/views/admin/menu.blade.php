451e617587659a2d3a86bd1b06782b28ebafaebd
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-08-24 08:48:03 +0300

    added link to service

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -31,13 +31,13 @@
             </div>
 
             <ul class="nav navbar-nav side-bar">
-                <li class="side-bar tmargin"><a href="#"><span class="fa fa-car">&nbsp;</span> Cars</a></li>
+                <li class="side-bar tmargin"><a href="{{ route('app.carpark.index') }}"><span class="fa fa-car">&nbsp;</span> Cars</a></li>
                 <li class="side-bar">
-                    <a href="#"><span><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;</span> Drivers</a></li>
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-tint">&nbsp;</span>Fuel</a></li>
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-road">&nbsp;</span>Routes</a></li>
+                    <a href="{{ route('app.drivers.index') }}"><span><i class="fa fa-id-card-o" aria-hidden="true"></i>&nbsp;</span> Drivers</a></li>
+                <li class="side-bar"><a href="{{ route('app.fuel.index') }}"><span class="glyphicon glyphicon-tint">&nbsp;</span>Fuel</a></li>
+                <li class="side-bar"><a href="{{ route('app.routes.index') }}"><span class="glyphicon glyphicon-road">&nbsp;</span>Routes</a></li>
 
-                <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-user">&nbsp;</span>Users</a></li>
+                <li class="side-bar"><a href="{{ route('app.users.index') }}"><span class="glyphicon glyphicon-user">&nbsp;</span>Users</a></li>
                 <li class="side-bar"><a href="#"><span class="glyphicon glyphicon-signal">&nbsp;</span>Statistics</a></li>
 
             </ul>
